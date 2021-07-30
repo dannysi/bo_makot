0a879f66f1177d00e0fc3b3e59b64e928d48d1c2
dannysi <danny.sivan@gmail.com>
2021-07-30 10:43:47 +0300

    basic chat is done

--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import store from "../Store";
 import chatPageStore from "./ChatPageStore"
 import "./ChatPage.scss";
+import { observe } from "mobx";
+import { observer } from "mobx-react";
 
 const isMe = (user:string) => user === store.userName
 
@@ -10,26 +12,39 @@ const getMessageCls = (user: string) => {
     return ['message', selfCls, "with-arrow"].join(' ');
 }
 
-const createMessageCard = (msg:string, user:string) => {
+const createMessageCard = (msg:string, user:string, key:number) => {
     const msgCls = getMessageCls(user);
     return (
-      <div className={msgCls}>
+      <div className={msgCls} key={key}>
         <div className="username">{user}</div>
         <div>{msg}</div>
       </div>
     )
 }
-    
 
-export default function ChatPage() {
+interface Message {
+    msg: string;
+    user: string;
+}
+
+const channel = "test_channel"
+const recieverBc = new BroadcastChannel(channel);
+// TODO convert to class 
+function ChatPage() {
     const [newMsg, setNewMsg] = React.useState('');
     const fieldRef = React.useRef<HTMLDivElement>(null);
-    const enterMessage = (e, user) => {
+    const myMessage =(e, user) => {
         e.preventDefault();
-        const elem = createMessageCard(newMsg, user)
+        // const bc = new BroadcastChannel(channel);
+        const sendData = {msg:newMsg, user:user}
+        recieverBc.postMessage(sendData);
+        enterMessage(sendData); 
+    }
+    const enterMessage = ({msg, user}:Message) => {
+        const elem = createMessageCard(msg, user, chatPageStore.allMsgs.length) 
         chatPageStore.addMsg(elem);
         setNewMsg("");
-        console.log(chatPageStore.allMsgs)        
+        
     }
     const messagesComp = <div className="messages" ref={fieldRef}>{chatPageStore.allMsgs}</div>
     
@@ -38,12 +53,19 @@ export default function ChatPage() {
             behavior: "smooth",
         })
     });
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            recieverBc.onmessage = (msg) => enterMessage(msg.data)
+        }, 1000)
+        return () => clearInterval(interval);
+    },[]);
+        
 
     return (
         <div className="chat">
             {messagesComp} 
             <div className="footer">
-                <form className="controls" onSubmit={(msg) => enterMessage(msg, store.userName)}>
+                <form className="controls" onSubmit={(msg) => myMessage(msg, store.userName)}>
                     <input placeholder="Fight!" value={newMsg} onChange={(e) => {setNewMsg(e.target.value)} }/>
                     <button type="submit">Send</button>
                 </form>
@@ -51,3 +73,5 @@ export default function ChatPage() {
         </div>
     )
 }
+
+export default observer(ChatPage)
